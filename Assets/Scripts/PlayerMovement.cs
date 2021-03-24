ddd90e2bf5cdc9e2151e3f38011baaeba2b7f5d5
4rfel <38050598+4rfel@users.noreply.github.com>
2021-03-24 18:41:39 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+public class PlayerMovement : NetworkedBehaviour {
+
+    Rigidbody2D rb;
+
+    public float hp;
+
+    public float g = 10;
+
+    float speed_mult = 5;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        hp = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsLocalPlayer)
+        {
+            MovePlayer();
+        }
+    }
+
+    void MovePlayer() {
+        Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), 0f);
+        dir = Vector2.ClampMagnitude(dir, 1f);
+        rb.velocity = dir * speed_mult;
+    }
+}
