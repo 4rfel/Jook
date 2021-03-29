d104362883b53d9d02cdf469323f7341cf2275d9
4rfel <38050598+4rfel@users.noreply.github.com>
2021-03-28 22:10:47 -0300

    z

--- Assets/Scripts/Player/LightController.cs
+++ Assets/Scripts/Player/LightController.cs
@@ -9,38 +9,36 @@ public class LightController : NetworkedBehaviour {
     [SerializeField] private SpriteRenderer playerRend;
     [SerializeField] private SpriteRenderer gunRend;
 
-    private bool isLightOn;
-    private bool wasLightOn;
+	private bool isLightOn;
+	private bool wasLightOn;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (IsLocalPlayer)
-        {
-            isLightOn = false;
-            wasLightOn = false;
-            playerRend.maskInteraction = SpriteMaskInteraction.None;
-            gunRend.maskInteraction = SpriteMaskInteraction.None;
-        }
-        ChangeLightState();
-    }
+	//  // Start is called before the first frame update
+	void Start() {
+		if ( IsLocalPlayer ) {
+			isLightOn = false;
+			wasLightOn = false;
+			playerRend.maskInteraction = SpriteMaskInteraction.None;
+			gunRend.maskInteraction = SpriteMaskInteraction.None;
+		}
+		ChangeLightState();
+	}
 
-    // Update is called once per frame
-    void Update() {
-        if (IsLocalPlayer) {
+	// Update is called once per frame
+	void Update() {
+		if ( IsLocalPlayer ) {
 			isLightOn = Input.GetMouseButton(1);
-        }
-        if (wasLightOn != isLightOn)
-            ChangeLightState();
-    }
+		}
+		if ( wasLightOn != isLightOn )
+			ChangeLightState();
+	}
 
-    void ChangeLightState() {
-        if (isLightOn) {
+	void ChangeLightState() {
+		if ( isLightOn ) {
 			spotlight.SetActive(true);
-            wasLightOn = true;
+			wasLightOn = true;
 		} else {
-            spotlight.SetActive(false);
-            wasLightOn = false;
-        }
-    }
+			spotlight.SetActive(false);
+			wasLightOn = false;
+		}
+	}
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,60 +5,47 @@ using MLAPI;
 
 public class PlayerMovement : NetworkedBehaviour {
 
+    [SerializeField] LayerMask Ground;
 
     Rigidbody2D rb;
-    
-    float speed_mult = 10f;
+
     float max_speed = 10f;
     float jumpMult = 500;
-
-    bool hasJumped = false;
-
+    
     void Start() {
-        if (IsLocalPlayer)
-        {
+        if ( IsLocalPlayer ) {
             rb = GetComponent<Rigidbody2D>();
             MovePlayer();
         }
     }
 
     void Update() {
-        if (IsLocalPlayer)
-        {
+        if ( IsLocalPlayer ) {
             MovePlayer();
-            if(Input.GetKeyDown("space"))
+
+            if ( Input.GetKeyDown("space") )
                 Jump();
         }
     }
 
     void MovePlayer() {
-        Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), 0f);
-        rb.AddForce(dir * speed_mult);
-
-        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -max_speed, max_speed), rb.velocity.y);
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * max_speed, rb.velocity.y);
     }
 
     void Jump() {
-        if (!hasJumped)
-        {
-            rb.AddForce(Vector2.up * jumpMult);
-            //hasJumped = true;
-        }
-    }
+		if ( CheckGround() ) {
+			rb.AddForce(Vector2.up * jumpMult);
+            Debug.Log("AAAA");
+		}
+	}
 
-    void OnCollisionEnter(Collision col) {
-        // Print how many points are colliding with this transform
-        Debug.Log("Points colliding: " + col.contacts.Length);
-
-        // Print the normal of the first point in the collision.
-        Debug.Log("Normal of the first point: " + col.contacts[0].normal);
-
-        foreach (var item in col.contacts) {
-            if (item.normal.x != -1f) {
-                hasJumped = false;
-				break;
-            }
-            Debug.DrawRay(item.point, item.normal * 100, Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f), 10f);
-        }
-    }
+    bool CheckGround() {
+        RaycastHit2D hit;
+        hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, Ground);
+        if ( hit.collider != null ) {
+            return true;
+		} else {
+            return false;
+		}
+	}
 }
