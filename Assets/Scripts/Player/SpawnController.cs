9de1eef516dbc5240cf58d63c4bd0b67a2cdca09
4rfel <38050598+4rfel@users.noreply.github.com>
2021-04-07 18:04:16 -0300

    add spawn point

---
+++ Assets/Scripts/Player/SpawnController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using MLAPI;
+
+public class SpawnController : NetworkedBehaviour {
+    void Start() {
+        if (IsLocalPlayer) {
+            GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+
+            GetComponent<Transform>().position = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;   
+        }
+    }
+}
