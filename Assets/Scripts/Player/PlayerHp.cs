2caacaef01fad45c1eacf19d5c6763834f89f549
4rfel <38050598+4rfel@users.noreply.github.com>
2021-04-07 21:26:32 -0300

    add respawn

--- Assets/Scripts/Player/PlayerHp.cs
+++ Assets/Scripts/Player/PlayerHp.cs
@@ -1,11 +1,28 @@
 ﻿using MLAPI;
+using MLAPI.Messaging;
 using MLAPI.NetworkedVar;
+using UnityEngine;
 
 public class PlayerHp : NetworkedBehaviour {
+
 	public NetworkedVarFloat hp = new NetworkedVarFloat(100f);
 
+	GameObject[] spawnPoints;
+
+	private void Start() {
+		spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+	}
 
 	public void TakeDmg(float dmg) {
 		hp.Value -= dmg;
+		if(hp.Value < 0) {
+			hp.Value = 100f;
+			InvokeClientRpcOnEveryone(Respawn);
+		}
+	}
+
+	[ClientRPC]
+	void Respawn() {
+		GetComponent<Transform>().position = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
 	}
 }
