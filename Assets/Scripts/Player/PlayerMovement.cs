9b8fefc1e10cb458e581af02f09dd94ba8cb57e7
Thomas Queiroz <thomasqueirozb@gmail.com>
2021-04-18 18:29:04 -0300

    Remove Debug.Log

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,65 +3,63 @@ using MLAPI;
 
 public class PlayerMovement : NetworkedBehaviour {
 
-	[SerializeField] LayerMask Ground;
+    [SerializeField] LayerMask Ground;
 
-	Rigidbody2D rb;
+    Rigidbody2D rb;
 
-	float max_speed = 10f;
-	public float jumpMult = 10;
+    float max_speed = 10f;
+    public float jumpMult = 10;
 
-	void Start() {
-		if (IsLocalPlayer) {
-			rb = GetComponent<Rigidbody2D>();
-			MovePlayer();
-		}
-	}
+    void Start() {
+        if (IsLocalPlayer) {
+            rb = GetComponent<Rigidbody2D>();
+            MovePlayer();
+        }
+    }
 
-	void Update() {
-		if (IsLocalPlayer) {
-			MovePlayer();
+    void Update() {
+        if (IsLocalPlayer) {
+            MovePlayer();
 
-			if (Input.GetKeyDown("space"))
-				Jump();
-		}
-	}
+            if (Input.GetKeyDown("space"))
+                Jump();
+        }
+    }
 
-	void MovePlayer() {
-		rb.velocity = new Vector2(Input.GetAxis("Horizontal") * max_speed, rb.velocity.y);
-	}
+    void MovePlayer() {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * max_speed, rb.velocity.y);
+    }
 
-	void Jump() {
-		if (CheckGround()) {
-			rb.velocity = new Vector2(rb.velocity.x, jumpMult);
-		} else if (CheckRightWall()) {
-			//rb.AddForce()
-			rb.velocity = new Vector2(rb.velocity.x, jumpMult);
+    void Jump() {
+        if (CheckGround()) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpMult);
+        } else if (CheckRightWall()) {
+            //rb.AddForce()
+            rb.velocity = new Vector2(rb.velocity.x, jumpMult);
 
-			//rb.velocity = new Vector2(rb.velocity.x - 20, jumpMult);
-		} else if (CheckLeftWall()) {
-			rb.velocity = new Vector2(rb.velocity.x, jumpMult);
+            //rb.velocity = new Vector2(rb.velocity.x - 20, jumpMult);
+        } else if (CheckLeftWall()) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpMult);
 
-			//rb.velocity = new Vector2(rb.velocity.x + jumpMult, jumpMult);
-		}
-	}
+            //rb.velocity = new Vector2(rb.velocity.x + jumpMult, jumpMult);
+        }
+    }
 
-	bool CheckGround() {
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, Ground);
+    bool CheckGround() {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, Ground);
 
-		Debug.Log(hit.collider);
+        return (hit.collider != null);
+    }
 
-		return (hit.collider != null);
-	}
+    bool CheckRightWall() {
+        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, 0.6f, Ground);
 
-	bool CheckRightWall() {
-		RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, 0.6f, Ground);
+        return (hitRight.collider != null);
+    }
 
-		return (hitRight.collider != null);
-	}
+    bool CheckLeftWall() {
+        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, 0.6f, Ground);
 
-	bool CheckLeftWall() {
-		RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, 0.6f, Ground);
-
-		return (hitLeft.collider != null);
-	}
+        return (hitLeft.collider != null);
+    }
 }
